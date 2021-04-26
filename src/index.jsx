c7b2dc27afa67645990fdb89ef972298f5b7aede
konstantinkirieri <kirieris@mail.ru>
2021-04-27 00:09:57 +0300

    home work 6

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { ConnectedRouter } from 'connected-react-router';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import initStore, { history } from './utils/store';
 import Router from './component/Router';
 
+const { store, persistor } = initStore();
+
 ReactDom.render(
-    <Provider store={initStore()}>
+    <Provider store={store}>
+        <PersistGate loading={ null } persistor={ persistor }>
         <ConnectedRouter history={history}>
             <Router/>
         </ConnectedRouter>
+        </PersistGate>
     </Provider>,
     document.getElementById('app')
 )
